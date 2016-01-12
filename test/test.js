ab28d819ef8aeb16d8c7fb810ba6fbdda1349213
SamHeutmaker <samheutmaker@gmail.com>
2016-01-11 22:34:00 -0800

    Fixed problems with mocha, cleaned up directories

--- test/test.js
+++ test/test.js
@@ -1,27 +1,10 @@
-var client = require(__dirname + "index.js");
-var helpers = require(__dirname + "/node_modules/bitmap-helpers/index.js");
-var tranformers = require(__dirname + "/node_modules/bitmap-helpers/node_modules/bitmap-transformationsindex.js");
+const client = require(__dirname + "/../index.js");
+const helpers = require(__dirname + "/../node_modules/bitmap-helpers/index.js");
+const transformers = require(__dirname + "/../node_modules/bitmap-helpers/node_modules/bitmap-transformations/index.js");
+const expect = require('chai').expect;
 
-var expect = require('chai').expect;
-
-
-
-describe('The buffer' , () => {
-	it('should load its length' , () => {
-		expect(index(test)).to.eql("")
-	})
-
-});
-
-describe('The helpers' , () => {
-	it('should ' , () => {
-		expect( )
-	});
-});
-
-
-describe('The tranformers: ' , () => {
-	it('should return grey scale properly:' , () => {
-		expect( transformers.greyScale([10,20,30]) ).to.eql([20,20,20]);
+describe('The tranformers' , () => {
+	it('should return grey scale properly' , () => {
+		expect( transformers.grayscale([10,20,30]) ).to.eql([20,20,20]);
 	})
 });
